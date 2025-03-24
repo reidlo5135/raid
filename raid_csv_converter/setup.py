5a382550f079a47e17de7a206f441a7a15907b12
reidlo5135 <201840103@email.daelim.ac.kr>
2025-03-24 07:15:20 +0000

    feat : initial commit

--- raid_csv_converter/setup.py
+++ raid_csv_converter/setup.py
@@ -1,25 +1,30 @@
+import os
 from setuptools import find_packages, setup
+from glob import glob
 
-package_name = 'raid_csv_converter'
+package_name: str = "raid_csv_converter"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(include=[package_name, f"{package_name}.*"], exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (os.path.join("share", package_name, "config"), glob("config/*.yaml")),
+        (os.path.join("share", package_name, "resource"), glob("resource/*")),
+        (os.path.join("share", package_name), glob("launch/*launch.[pxy][yma]*"))
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='reidlo',
-    maintainer_email='201840103@email.daelim.ac.kr',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="reidlo",
+    maintainer_email="201840103@email.daelim.ac.kr",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
+        "console_scripts": [
+            f"{package_name} = {package_name}.main:main"
         ],
     },
 )
